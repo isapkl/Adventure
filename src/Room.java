0636221c48c2adca3ecd57ccb351b2a10578df90
isapkl <isabellapkl@gmail.com>
2026-09-23 09:09:02 +0200

    Room start

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,27 @@
 public class Room {
+    private String name;
+    private String description;
+
+    private Room north;
+    private Room east;
+    private Room south;
+    private Room west;
+
+    public Room(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getDescription(){
+        return description;
+    }
+
+    public void setNorth(Room room){
+
+    }
+    public Room getNorth(){
+    }
 }
